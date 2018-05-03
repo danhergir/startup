907b3279d7e024bd7c5eb4f0ded46bb5d07a0a32
Daniel  Hernandez <danhergir@MacBook-Air-de-Daniel.local>
2018-05-02 19:45:32 -0500

    "Extending Layout and adding Bootsrap and jQuery "

--- resources/views/welcome/index.blade.php
+++ resources/views/welcome/index.blade.php
@@ -1,9 +1,5 @@
-<div class="row">
-    <div class="container">
-        @foreach($products as $product)
-        <p class="description">
-            {{ $product->description }}
-        </p>
-        @endforeach
-    </div>
-</div>
+@extends('layouts.app')
+
+@section('content')
+    
+@endsection
