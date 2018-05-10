09864ce62d72fb99c3e496d1c7cc7d8f18678f1a
Daniel  Hernandez <danhergir@MacBook-Air-de-Daniel.local>
2018-05-10 00:10:38 -0500

    Header, Almost Done

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,7 +1,34 @@
 <div class="row">
-    <div class="col-md-12 bg-primary navbar navbar-expand-lg">
-        <div class="col-md-3">
-            
+    <div class="col-md-12 navbar navbar-expand-lg navbar-light bg-primary">
+        <div class="col-md-3 ml-2">
+            <span><a class="navbar-brand" href="{{ route('welcome.index') }}">Brand</a></span>
+        </div>
+        <div class="col-md-6">
+            <div class="input-group">
+                <div class="input-group-prepend rounded-left bg-light" style="overflow:hidden">
+                    <button class="btn btn-outline-dark dropdown-toggle" style="height:50px; border:hidden" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fas fa-bars"></i></button>
+                    <div class="dropdown-menu">
+                    <a class="dropdown-item" href="#">Monitors</a>
+                    <a class="dropdown-item" href="#">Videos Games</a>
+                    <a class="dropdown-item" href="#">Computers</a>
+                    <a class="dropdown-item" href="#">More Tech</a>
+                    </div>
+                <form class="form-inline my-2 my-lg-0 bg-light rounded-right" style:"outline:hidden; overflow:hidden">
+                <input style="height: 50px; width: 450px;" type="text" placeholder="Search" class="form-control" aria-label="Text input with dropdown button">
+                <button style="height: 50px" class="btn btn-ligh my-2 my-sm-0" type="submit"><i class="fa fa-search"></i></button>
+                </form>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-2">
+            <span class="user-account"><a class="navbar-brand" href="#"><i class="fas fa-user"></i></a></span>
+            <span class="user-account"><a class="navbar-brand ml-4" href="#"><i class="fas fa-shopping-cart"></i></a></span>
+            <span class="user-account"><a class="navbar-brand ml-4" href="#"><i class="fas fa-heart"></i></a></span>
         </div>
     </div>
-</div>
+</div>   
+
+
+
+    
+  
--- resources/views/welcome/index.blade.php
+++ resources/views/welcome/index.blade.php
@@ -1,1 +1,9 @@
 @extends('layouts.app')
+
+@section('title')
+    Brand
+@endsection
+
+@section('content')
+    
+@endsection
