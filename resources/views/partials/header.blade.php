f91456e12dc1c77141576d3128fee4244e9cedc7
Daniel  Hernandez <danhergir@MacBook-Air-de-Daniel.local>
2018-05-09 22:51:33 -0500

    Re-change Index, Show And Header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,24 +1,7 @@
-<nav class="navbar sticky-top navbar-expand-lg navbar-light bg-primary">
-    <a class="navbar-brand" href="#">Brand</a>
-    
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-        <li class="nav-item active">
-            <a class="nav-link" href="#"><i class="fas fa-user"></i><span class="sr-only">(current)</span> User</a>
-        </li>
-        <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Categories
-            </a>
-            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <a class="dropdown-item" href="#">Technology</a>
-            <a class="dropdown-item" href="#">Electronics</a>
-            <a class="dropdown-item" href="#">Music & Videos</a>
-        </ul>
+<div class="row">
+    <div class="col-md-12 bg-primary navbar navbar-expand-lg">
+        <div class="col-md-3">
+            
+        </div>
     </div>
-        
-    <form class="form-inline my-2 my-lg-0 justify-content-center">
-    <input style="width: 400px;" class="form-control mr-sm-2 rounded-input" type="search" placeholder="Search" aria-label="Search">
-    <button class="btn btn-primary my-2 my-sm-0 rounded-input" type="submit"><i class="fas fa-search"></i></button>
-    </form>
-</nav>
+</div>
